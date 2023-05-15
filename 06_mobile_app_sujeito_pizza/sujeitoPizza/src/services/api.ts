35a96a44baee1c2b393505b0a98b38c89c784367
WeltonAM <74322996+WeltonAM@users.noreply.github.com>
2023-05-15 10:38:28 -0300

    Advancing
    Environment variables config

--- 06_mobile_app_sujeito_pizza/sujeitoPizza/src/services/api.ts
+++ 06_mobile_app_sujeito_pizza/sujeitoPizza/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { BASE_URL } from '@env';
 
 const api = axios.create({
-    // baseURL: 'http://192.168.0.152:3333'
+    baseURL: BASE_URL
 });
 
 export { api };
